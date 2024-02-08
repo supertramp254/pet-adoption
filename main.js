54d688aa03f76b5557db7d50ebec0ff6279c510e
Francesco <zaccagninifrancesco@gmail.com>
2024-02-08 13:38:47 +0100

    finish pet filter

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ async function petsArea() {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
 
+    clone.querySelector(".pet-card").dataset.species = pet.species;
+
     clone.querySelector("h3").textContent = pet.name;
     clone.querySelector(".pet-description").textContent = pet.description;
 
@@ -51,4 +53,28 @@ function createAgeText(birthYear) {
   if (age == 0) return "Less than a year old";
 
   return `${age} years old`;
+}
+
+// pet filter button code
+
+const allButtons = document.querySelectorAll(".pet-filter button");
+
+allButtons.forEach((el) => {
+  el.addEventListener("click", handleButtonClick);
+});
+
+function handleButtonClick(e) {
+  // remove active class from any and all buttons
+  allButtons.forEach((el) => el.classList.remove("active"));
+  // add active class to the specific button that just got clicked
+  e.target.classList.add("active");
+  // actually filter the pets down below
+  const currentFilter = e.target.dataset.filter;
+  document.querySelectorAll(".pet-card").forEach((el) => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid";
+    } else {
+      el.style.display = "none";
+    }
+  });
 }
